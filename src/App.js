6266476d9f04e51d1ecfce21bd876dce2a3a95f4
Sarabjeet Singh <sarabjeet0601@gmail.com>
2023-05-27 14:18:49 +0530

    skills section added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import Contributions from './components/Contributions';
+import ProjectHelpSection from './components/ProjectHelpSection';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <HeroSection/>
+      <Contributions/>
+      <ProjectHelpSection/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ProjectHelpSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const ProjectSection = () =>{
+    return(
+        <div className='ProjectSectionContainer'>
+            <h1>Get Project Assistance</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni officiis repellendus sunt necessitatibus, repellat aliquam aspernatur, maiores debitis obcaecati voluptate culpa! Repellat similique incidunt, pariatur aperiam, quidem eveniet itaque consequuntur vitae, sint ratione molestias fugit dolorum eius non? Nemo culpa consequatur quod voluptates facilis ut temporibus quis voluptatum pariatur!</p>
+        </div>
+    )
+}
+
+export default ProjectSection
