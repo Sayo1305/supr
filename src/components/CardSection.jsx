84d1fb13a88ba840a1a318258a272e6f7d400d28
Palakmidha <palakmidha54@gmail.com>
2023-05-29 16:57:59 +0530

    card section updated

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -3,45 +3,86 @@ import Card1 from "../assets/images/card1.jpg";
 import Card2 from "../assets/images/card2.jpg";
 import Card3 from "../assets/images/card3.jpg";
 import Card4 from "../assets/images/card4.jpg";
+// import { Link } from "react-router-dom";
 
 const CardSection = () => {
 return(
-    <div className="cardSectionContainter">
-    <div className="container">
-        <div className="card">
-            <img src={Card1} alt=""/>
-            <div className="intro">
-                <h2>1 on 1 Call</h2>
-                <p>Book a 1 on 1 session with your favorite mentor.
-                </p>
-            </div>
-        </div>
+    // <div className="cardSectionContainter">
+    // <div className="container">
+    //     <div className="card">
+    //         <img src={Card1} alt=""/>
+    //         <div className="intro">
+    //             <h2>1 on 1 Call</h2>
+    //             <p>Book a 1 on 1 session with your favorite mentor.
+    //             </p>
+    //         </div>
+    //     </div>
 
-        <div className="card">
-            <img src={Card2} alt=""/>
-            <div className="intro">
-                <h2>Resources</h2>
-                <p>get amazing content at just one click and that too free of cost.</p>
-            </div>
-        </div>
+    //     <div className="card">
+    //         <img src={Card2} alt=""/>
+    //         <div className="intro">
+    //             <h2>Resources</h2>
+    //             <p>get amazing content at just one click and that too free of cost.</p>
+    //         </div>
+    //     </div>
 
-        <div className="card">
-            <img src={Card3} alt=""/>
-            <div className="intro">
-                <h2>Project Contribution</h2>
-                <p>Struggling with your ongoing personal project? get help.
-                </p>
-            </div>
-        </div>
+    //     <div className="card">
+    //         <img src={Card3} alt=""/>
+    //         <div className="intro">
+    //             <h2>Project Contribution</h2>
+    //             <p>Struggling with your ongoing personal project? get help.
+    //             </p>
+    //         </div>
+    //     </div>
 
-        <div className="card">
-            <img src={Card4} alt=""/>
-            <div className="intro">
-                <h2>Video Contribution</h2>
-                <p>do something for the community and add your valuable skills.
-                </p>
+    //     <div className="card">
+    //         <img src={Card4} alt=""/>
+    //         <div className="intro">
+    //             <h2>Video Contribution</h2>
+    //             <p>do something for the community and add your valuable skills.
+    //             </p>
+    //         </div>
+    //     </div>
+    // </div>
+    // </div>
+
+
+
+    <div className="heading">OUR PRODUCTS
+        <div className="container">
+            <div className="card">
+                <img src={Card1} alt="pets"/>
+                <div className="intro">
+                    <h1>1 on 1 Call</h1>
+                    <h3>Book a 1 on 1 session with your favorite mentor.</h3>
+                    {/* <button><Link to={'/groomingproducts'}>SHOP NOW</Link></button> */}
+                </div>
             </div>
-        </div>
+            <div className="card">
+                <img src={Card2} alt="food"/>
+                <div className="intro">
+                    <h1>Resources</h1>
+                    <h3>Get amazing content at just one click and that too free of cost.</h3>
+                    {/* <button><Link to={'/foodproducts'}>SHOP NOW</Link></button> */}
+                </div>
+            </div>
+            <div className="card">
+                <img src={Card3} alt="toys"/>
+                <div className="intro">
+                    <h1>Project Contribution</h1>
+                    <h3>Struggling with your ongoing personal project? get help.</h3>
+                    {/* <button><Link to={'/accessories'}>SHOP NOW</Link></button> */}
+                </div>
+                </div>
+
+                <div className="card">
+                <img src={Card4} alt="toys"/>
+                <div className="intro">
+                    <h1>Video Contribution</h1>
+                    <h3>Do something for the community and add your valuable skills.</h3>
+                    {/* <button><Link to={'/accessories'}>SHOP NOW</Link></button> */}
+                </div>
+                </div>
     </div>
     </div>
 );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import '../assets/css/signup.css';
+import {Link} from "react-router-dom";
 
 const Signup = () => {
   return (
     <div className="signupContainer">
         <div className="signupBox">
-
+        <div className="welcome">
+          WELCOME
+        </div>
+        <div className="inputContainer">
+          <input type="text" placeholder="First Name" className="box"/>
+          <input type="text" placeholder="Last Name" className="box"/>
+          <input type="text" placeholder="Email" className="box"/>
+          <input type="password" placeholder="Password" className="box"/>
+        </div>
+        <div className="buttonContainerSignup">
+          <button content="Login" className="btn2">Signup</button>
+        </div>
+        <div className="alreadyAccount">
+          <p>Already have a account? <Link to={'/login'} className='loggedin'>Login</Link></p>
+          
+        </div>
         </div>
     </div>
   )
