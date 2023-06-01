76cda3386d4b6aacd8fc6c0d123946bff811ba8f
Unnat Das <udas4153@gmail.com>
2023-06-01 21:22:23 +0530

    added mentor page

--- src/pages/Mentor.jsx
+++ src/pages/Mentor.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../assets/css/mentor.css";
-import MentorCard from '../components/MentorCard';
+import MentorCard from "../components/MentorCard";
+import MentorFormModal from "../components/MentorFormModal";
 const Mentor = () => {
+  const [openmodal, setopenmodal] = useState(false);
   return (
-    <div className='MentorContainer'>
-      <div className='MentorHead'>Chat or Book 1 on 1 Session with our Mentors</div>
-      <div className='MentorGridCont'>
-            <MentorCard/>
-            <MentorCard/>
-            <MentorCard/>
-            <MentorCard/>
-            <MentorCard/>
-            <MentorCard/>
-            <MentorCard/>
+    <>
+    <MentorFormModal openmodal={openmodal} setopenmodal={setopenmodal}/>
+      <div className="MentorContainer">
+        <div className="MentorHead">
+          Chat or Book 1 on 1 Session with our Mentors
+          <div
+            onClick={() => {
+              setopenmodal(!openmodal);
+            }}
+            className="BecomeMentorButton"
+          >
+            Be A Mentor
+          </div>
+        </div>
+        <div className="MentorGridCont">
+          <MentorCard />
+          <MentorCard />
+          <MentorCard />
+          <MentorCard />
+          <MentorCard />
+          <MentorCard />
+          <MentorCard />
+        </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Mentor
+export default Mentor;
