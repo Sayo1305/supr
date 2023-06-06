a64c9460b7c14ecdc6da371940f9e51bbf190a37
Palakmidha <palakmidha54@gmail.com>
2023-06-06 16:33:58 +0530

    random

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,7 +10,6 @@ const Profile = () => {
       <div className="imgContainer">
         <img src={ProfileImg} className="pImg" alt="profile"/>
         <h3 className="username">Username</h3>
-        
       </div>
       <div className="rightSide">
         hello 
