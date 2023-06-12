81bdbf1130f8c204b2497fd52bd73aec43c75b2e
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-06-12 19:43:22 +0530

    added resource upload option

--- src/pages/ResourceDescPage.jsx
+++ src/pages/ResourceDescPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import '../assets/css/ResourceDescPageCSS.css'
 import ReactPic from '../assets/images/react_img.png'
+import { GrAdd } from 'react-icons/gr'
+import ResourcePageModal from '../components/ResourcePageModal'
+import { useState } from 'react'
 
 const ResourceDescPage = () => {
+  const [openmodal, setopenmodal] = useState('');
   return (
+    <div>
+      <ResourcePageModal openmodal={openmodal} setopenmodal={setopenmodal}/>
     <div className='ResourcePageDesc_Conatiner'>
+
       <div className="ResDesc">
-        <button>Add resources</button>
+      <button onClick={() =>{
+            setopenmodal(!openmodal);
+          }} > <GrAdd/> Add Resources</button>
         <h1>React Resources</h1>
         <img src={ReactPic} alt="" />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus incidunt! Facilis perspiciatis veniam maiores nobis doloremque laborum animi sapiente eligendi sunt! Esse nesciunt minus cupiditate aperiam vitae perferendis? Facere eligendi at incidunt ipsum sunt! Repellendus quia, beatae numquam earum in, laudantium deleniti vel dolor, quam atque nihil. Ad deserunt accusantium aliquid nulla, non debitis explicabo labore necessitatibus maxime culpa voluptates, perspiciatis dicta sed aperiam unde dolore? In numquam cumque reprehenderit cupiditate accusantium amet? Aliquid omnis maxime vitae et saepe ducimus fuga adipisci, itaque quia nulla ea in deserunt natus est quis culpa, impedit vero beatae dignissimos odio consequatur deleniti.</p>
@@ -16,6 +25,7 @@ const ResourceDescPage = () => {
         <button>Notes</button>
         <button>Official Docs</button>
       </div>
+    </div>
     </div>
   )
 }
